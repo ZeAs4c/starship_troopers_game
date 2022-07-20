c87756be89052f1d3817c45e31e97916ab73b720
ZeAs4c <alex.kirillov1@yandex.ru>
2022-07-20 17:30:54 +0300

    Первичная инициализация игры

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'game_core/main_loop.dart';
 
 void main() {
-  startLoop();
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Настройка правильной иницилизации виджетов
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+    SystemChrome.setEnabledSystemUIOverlays([
+      SystemUiOverlay.bottom // скрываем строку состояний
+    ]);
+    SystemChrome.setEnabledSystemUIOverlays(
+        []); // скрываем нижнюю панель андроид
+
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false, // Убираем банер в правом верхнем углу
+      home: Center(child: Text("STARSHIP TROOPERS")),
+    ));
+  }); // SystemChrome - позволяет обращаться к низко-уровневым библам, устанавливаем наше приложение в горизонтальный вид
+  // Так как setPreferredOrientations работает в ассинхронном режиме, то с помощью whenComplete, скажем чтобы runApp выполнялась после
+  // переворота экрана в горизонтальный вид
 }
